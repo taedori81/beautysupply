257dbbc3117240c246adf4754ae409a202ed3186
taedori <taedori@outlook.com>
2015-12-09 16:06:55 -0800

    Fixed urls.py

--- allbeauty/settings/base.py
+++ allbeauty/settings/base.py
@@ -203,8 +203,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'shop', 'static')
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
